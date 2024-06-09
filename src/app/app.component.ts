30484a070fce17aeafcd7053b618272615caf4cd
JulyanaLozano <josephtemi@hotmail.com>
2024-06-09 10:46:37 -0500

    Fronted NORMCONT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'NORMCONT';
+  public title = 'Hola Mundo NORMCONT';
+  users: string[] = ['Juan Felipe','jfcordoba82@misena.edu.co','calle 16 # 13-48'];
+  /*users: string[] = [];*/
+  visible: boolean = false;
+
+  setVisible(): void {
+    this.visible = this.visible? false: true;
+    console.log('hemos echo clic aqui');
+  }
+
 }
